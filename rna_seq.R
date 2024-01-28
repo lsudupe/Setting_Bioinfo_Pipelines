1ec8b93a2521a1e37721a4b6c3f69062970e7b68
lsudupe <sudupelaura@gmail.com>
2024-01-28 17:29:15 +0300

    add further analysis

--- rna_seq.R
+++ rna_seq.R
@@ -63,4 +63,319 @@ data_NOISEQ <- readData(data = GSE198256_count_filt,
                         biotype= annotgene_ord$type ,
                         chromosome = annotgene_ord[,c("Chromosome","start","end")],
                         factors = Factors_GSE198256)
-data_NOISEQ
+# problems?
+myexplodata <- dat(data_NOISEQ, type = "biotype")
+explo.plot(myexplodata, plottype = "persample")
+mynicedata <- dat2save(myexplodata)
+mybiodetection <- dat(data_NOISEQ, k = 0, type = "biodetection", factor = NULL)
+
+
+lengthuse <- abs(annotgene_ord$end-annotgene_ord$start)
+names(lengthuse) <- rownames(annotgene_ord)
+gc <- annotgene_ord$GC
+names(gc) <- rownames(annotgene_ord)
+biotype <-annotgene_ord$type
+names(biotype) <- rownames(annotgene_ord)
+
+chromosome <- annotgene_ord[,c("Chromosome","start","end")]
+
+
+data_NOISEQ <- readData(data = GSE198256_count_filt,
+                        length=lengthuse,
+                        gc=gc,
+                        biotype= biotype ,
+                        chromosome = annotgene_ord[,c("Chromosome","start","end")],
+                        factors = Factors_GSE198256)
+
+myexplodata <- dat(data_NOISEQ, type = "biodetection")
+myexplodata <- dat(data_NOISEQ, type = "biodetection")
+
+## explore our data
+pdf("./results/RNAseq/GSE198256/GSE198256_type.pdf")
+print(explo.plot(myexplodata, plottype = "persample"))
+dev.off()
+
+pdf("./results/RNAseq/GSE198256/GSE198256_biotype_detection.pdf", width = 12, height = 8)
+par(mfrow = c(1, 2))
+explo.plot(myexplodata, samples = c(1, 2), toplot = "protein_coding", plottype = "comparison")
+dev.off()
+
+mycountsbio = dat(data_NOISEQ, factor = NULL, type = "countsbio")
+pdf("./results/RNAseq/GSE198256/GSE198256_each_expressionvalues.pdf", width = 10, height = 8)
+par(mfrow = c(1, 2))
+print(explo.plot(mycountsbio, toplot = 1, samples = 1, plottype = "boxplot"))
+dev.off()
+
+mysaturation = dat(data_NOISEQ, k = 0, ndepth = 7, type = "saturation")
+pdf("./results/RNAseq/GSE198256/GSE198256_sequencingdepth.pdf", width = 10, height = 8)
+par(mfrow = c(2, 1))
+explo.plot(mysaturation, toplot = 1, samples = 1:8, yleftlim = NULL, yrightlim = NULL)
+explo.plot(mysaturation, toplot = "protein_coding", samples = 1:8)
+dev.off()
+
+pdf("./results/RNAseq/GSE198256/GSE198256_cpm.pdf", width = 10, height = 8)
+explo.plot(mycountsbio, toplot = 1, samples = NULL, plottype = "barplot")
+dev.off()
+
+mylengthbias = dat(data_NOISEQ, factor = "Group", type = "lengthbias")
+pdf("./results/RNAseq/GSE198256/GSE198256_meanExpression_lengthBins.pdf", width = 10, height = 8)
+explo.plot(mylengthbias, samples = NULL, toplot = "global")
+dev.off()
+
+myGCbias = dat(data_NOISEQ, factor = "Group", type = "GCbias")
+pdf("./results/RNAseq/GSE198256/GSE198256_gc_content.pdf", width = 10, height = 8)
+explo.plot(myGCbias, samples = NULL, toplot = "global")
+dev.off()
+
+mycd = dat(data_NOISEQ, type = "cd", norm = FALSE, refColumn = 1)
+pdf("./results/RNAseq/GSE198256/GSE198256_cd_density.pdf", width = 10, height = 8)
+explo.plot(mycd,samples = 1:8)
+dev.off()
+
+myPCA = dat(data_NOISEQ, type = "PCA")
+pdf("./results/RNAseq/GSE198256/GSE198256_pca.pdf", width = 10, height = 8)
+explo.plot(myPCA, factor = "Group")
+dev.off()
+
+pdf("./results/RNAseq/GSE198256/GSE198256_qa.pdf", width = 10, height = 8)
+QCreport(data_NOISEQ, samples = NULL, factor = "Group", norm = FALSE)
+dev.off()
+
+## save 
+save(data_NOISEQ,GSE198256_count_filt,annotgene_ord,file="./data/GSE198256/GSE198256_step1.Rda")
+
+############
+## STEP 3: NORMALIZATION & DIFF EXPRESSION
+############
+
+myRPKM = rpkm(assayData(data_NOISEQ)$exprs, long = lengthuse, k = 0, lc = 1)
+myUQUA = uqua(assayData(data_NOISEQ)$exprs, long = lengthuse, lc = 0.5, k = 0)
+myTMM = tmm(assayData(data_NOISEQ)$exprs, long = 1000, lc = 0)
+
+############
+## STEP 3.1: DESEQ2
+############
+library(DESeq2)
+load("./data/GSE198256/GSE198256_step1.Rda")
+
+############
+# STEP 3.1.1: SET THE CLASS
+############
+
+GSE198256_DESeq2 <- DESeqDataSetFromMatrix(countData = GSE198256_count_filt,
+                                           colData = pData(data_NOISEQ),
+                                           design = ~ Group)
+# Warning
+pDataUSE <- pData(data_NOISEQ)
+pDataUSE[pDataUSE=="Control"] <- "control"
+pDataUSE[pDataUSE=="CFS"] <- "case"
+pDataUSE[,1] <- as.factor(pDataUSE[,1])
+pDataUSE$Group <- relevel(pDataUSE$Group, ref = "control")
+
+GSE198256_DESeq2 <- DESeqDataSetFromMatrix(countData = GSE198256_count_filt,
+                                           colData = pDataUSE,
+                                           design = ~ -1 + Group)
+resultsNames(GSE198256_DESeq2)
+GSE198256_DESeq2 <- DESeqDataSetFromMatrix(countData = GSE198256_count_filt,
+                                           colData = pDataUSE,
+                                           design = ~ Group)
+
+GSE198256_DESeq2
+
+############
+# STEP 3.1.2: WITH WHICH GENES TO WORK?
+############
+
+## Do we use all the genes?
+## How do we select which ones?
+
+smallestGroupSize <- 1
+keep <- rowSums(counts(GSE198256_DESeq2) >= 2) >= smallestGroupSize
+GSE198256_DESeq2_F <- GSE198256_DESeq2[keep,]
+
+
+############
+# STEP 3.1.3: DIFFERENTIAL EXPRESSION?
+############
+
+GSE198256_DESeq2_F<- DESeq(GSE198256_DESeq2_F)
+GSE198256_res <- results(GSE198256_DESeq2_F)
+GSE198256_res
+resultsNames(GSE198256_DESeq2_F)
+
+############
+# STEP 3.1.4: WE NEED TO UNDERSTAND MORE...
+############
+
+## Questions in my mind:
+# How do I define the question?
+# How the differential expression is done?
+# How to interpret the results?
+# Technical replicates?
+
+## STEP 3.1.4: plot MA
+
+#Interpretation?
+pdf("./results/RNAseq/GSE198256/GSE198256_MA.pdf", width = 10, height = 8)
+plotMA(GSE198256_res, ylim=c(-2,2))
+dev.off()
+
+lfcShrink(GSE198256_DESeq2_F,coef=c("Group_case_vs_control"))
+res_lfcShrink <- lfcShrink(GSE198256_DESeq2_F,coef=c("Group_case_vs_control"))
+
+pdf("./results/RNAseq/GSE198256/GSE198256_MA_lfc.pdf", width = 10, height = 8)
+plotMA(res_lfcShrink, ylim=c(-2,2))
+dev.off()
+
+GSE198256_res
+resOrdered <- GSE198256_res[order(GSE198256_res$pvalue),]
+summary(GSE198256_res)
+
+## STEP 3.1.4: Define questions
+
+resultsNames(GSE198256_DESeq2_F)
+
+## STEP 3.1.4: How differential expression is conducted...
+
+# DESeq2 offers two kinds of hypothesis tests: 
+#   the Wald test, 
+#        where we use the estimated standard error of a log2 fold 
+#        change to test if it is equal to zero, 
+#   the likelihood ratio test (LRT). 
+#        The LRT examines two models for the counts, a full model 
+#        with a certain number of terms and a reduced model, in 
+#        which some of the terms of the full model are removed. 
+#        The test determines if the increased likelihood of the 
+#        data using the extra terms in the full model is more 
+#        than expected if those extra terms are truly zero.
+
+resNorm <- lfcShrink(GSE198256_DESeq2_F, coef=2, type="normal")
+resAsh <- lfcShrink(GSE198256_DESeq2_F, coef=2, type="ashr")
+
+pdf("./results/RNAseq/GSE198256/GSE198256_MA_plots.pdf", width = 14, height = 5)
+par(mfrow=c(1,3), mar=c(4,4,2,1))
+xlim <- c(1,1e5); ylim <- c(-3,3)
+plotMA(res_lfcShrink, xlim=xlim, ylim=ylim, main="apeglm")
+plotMA(resNorm, xlim=xlim, ylim=ylim, main="normal")
+plotMA(resAsh, xlim=xlim, ylim=ylim, main="ashr")
+dev.off()
+
+## STEP 3.1.4: QC??
+
+# How do visualize?
+vsd <- vst(GSE198256_DESeq2_F, blind=FALSE)
+rld <- rlog(GSE198256_DESeq2_F, blind=FALSE)
+head(assay(vsd), 3)
+
+# this gives log2(n + 1)
+ntd <- normTransform(GSE198256_DESeq2_F)
+library("vsn")
+pdf("./results/RNAseq/GSE198256/GSE198256_effects_variation_transf1.pdf", width = 14, height = 5)
+meanSdPlot(assay(ntd))
+dev.off()
+
+pdf("./results/RNAseq/GSE198256/GSE198256_effects_variation_transf2.pdf", width = 14, height = 5)
+meanSdPlot(assay(vsd))
+dev.off()
+
+
+pdf("./results/RNAseq/GSE198256/GSE198256_effects_variation_transf3.pdf", width = 14, height = 5)
+meanSdPlot(assay(rld))
+dev.off()
+
+############
+# STEP 4: BIOLOGICAL INTERPRETATION
+############
+
+## heatmap
+library("pheatmap")
+select <- order(rowMeans(counts(GSE198256_DESeq2_F,normalized=TRUE)),
+                decreasing=TRUE)[1:20]
+ntd_data <- assay(ntd)[select,]
+
+df <- as.data.frame(colData(GSE198256_DESeq2_F)[,c("control","case")])
+pheatmap(assay(ntd)[select,], cluster_rows=FALSE, show_rownames=FALSE,
+         cluster_cols=FALSE, annotation_col=df)
+df <- as.data.frame(colData(GSE198256_DESeq2_F)[, "Group", drop = FALSE])
+
+# Plot the heatmap
+pdf("./results/RNAseq/GSE198256/GSE198256_heatmap.pdf", width = 10, height = 12)
+pheatmap(ntd_data, cluster_rows=FALSE, show_rownames=TRUE,
+         cluster_cols=TRUE, annotation_col=df)
+dev.off()
+#change names genes
+gene_names <- setNames(annotgene$Gene.name, annotgene$Gene.stable.ID)
+rownames(GSE198256_res) <- gene_names[rownames(GSE198256_res)]
+head(rownames(GSE198256_res))
+
+library(EnhancedVolcano)
+pdf("./results/RNAseq/GSE198256/GSE198256_volcano.pdf", width = 10, height = 8)
+EnhancedVolcano(GSE198256_res,
+                lab = rownames(GSE198256_res),
+                x = 'log2FoldChange',
+                y = 'pvalue',
+                xlim = c(-10,10))
+dev.off()
+
+## Gene Set Enrichment Analysis.
+#    a. ORA.
+#    b. GSEA
+# How do we bring all the information together at once?
+
+BiocManager::install("topGO")
+BiocManager::install("clusterProfiler")
+BiocManager::install("pathview")
+BiocManager::install("enrichplot")
+library(clusterProfiler)
+library(enrichplot)
+library(ggplot2)
+library(org.Hs.eg.db)
+library(geneAnnotations)
+library(exprAnalysis)
+
+results_anno <- GSE198256_res
+
+## cluster profiler
+OrgDb <- org.Hs.eg.db 
+
+geneList <- as.vector(results_anno$log2FoldChange)
+names(geneList) <- rownames(results_anno)
+gene <- as.vector(rownames(results_anno))
+
+# First, convert gene symbols to Entrez IDs
+entrez_ids <- AnnotationDbi::mapIds(OrgDb,
+                                    keys = gene,
+                                    column = "ENTREZID",
+                                    keytype = "SYMBOL",
+                                    multiVals = "first")
+
+
+entrez_ids_vector <- unlist(valid_entrez_ids)
+entrez_ids_vector <- as.character(entrez_ids_vector)
+head(entrez_ids_vector)
+
+# Group GO
+ggo <- clusterProfiler::groupGO(gene     = entrez_ids_vector,
+                                OrgDb    = OrgDb,
+                                ont      = "BP",
+                                level    = 3,
+                                readable = TRUE)
+head(summary(ggo)[,-5])
+
+pdf("./results/RNAseq/GSE198256/GSE198256_clusterprofiler_barplot.pdf", width = 10, height = 8)
+barplot(ego, showCategory=25)
+dev.off()
+
+# GO over-representation test
+ego <- clusterProfiler::enrichGO(gene          = entrez_ids_vector,
+                                 OrgDb         = OrgDb,
+                                 ont           = "BP",
+                                 pAdjustMethod = "BH",
+                                 pvalueCutoff  = 0.03,
+                                 qvalueCutoff  = 0.03, 
+                                 readable      = TRUE)
+head(summary(ego)[,-8])
+
+pdf("./results/RNAseq/GSE214282/GSE198256_clusterprofiler_ego.pdf", width = 8, height = 10)
+barplot(ego, showCategory=25)
+dev.off()
